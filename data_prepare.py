37bbe46cb8ad5b3bdca76119b319b001a64ee9f7
Ningning Zhao <49882520+ning22@users.noreply.github.com>
2020-10-27 11:30:40 +0800

    Update data_prepare.py

--- data_prepare.py
+++ data_prepare.py
@@ -1,3 +1,7 @@
+"""
+Ningning Zhao
+buaazhaonn@gmail.com
+"""
 import os
 from glob import glob 
 from sklearn.model_selection import KFold
@@ -41,58 +45,48 @@ def write2txt_v2(id_list_filename, txt_filename, image_list, mask_list):
     f.close()
 
 if __name__ == "__main__":
-    # train and test splitting v2
-    output_path = '/home/nzhao/Documents/PJ_Pelvis/main/data/hiatus'
+    output_path = 'data/data_name'
     os.makedirs(output_path, exist_ok=True)
-    # version = 'v2'
-    # data_folder = '/home/nzhao/Documents/PJ_Pelvis/image_pair_crop_'+version
-    # for stage in ['train', 'test']:
-    #     id_list_filename = os.path.join(output_path, 'id_'+stage+'_list.txt')
-    #     txt_filename = os.path.join(output_path, stage+'_list_' + version + '.txt')
-    #     image_list = sorted(glob(os.path.join(data_folder, '*_IMG*')))
-    #     label_list = sorted(glob(os.path.join(data_folder, '*_LBL*'))) 
-    #     write2txt_v2(id_list_filename, txt_filename, image_list, label_list)
-    # pdb.set_trace()
+    is_cross_validation = True
 
 
-    # train and test splitting
-    for stage in ['train', 'test']:
-    # for stage in ['test']:
-        data_folder = '/home/nzhao/Documents/PJ_Pelvis/image_pair_crop_' + stage 
-        output_path = '/home/nzhao/Documents/PJ_Pelvis/main/data/hiatus'
+    if not is_cross_validation:
+        for stage in ['train', 'test']:
+            data_folder = 'location of raw data' + stage
+        output_path = 'location of prepared data'
+            os.makedirs(output_path, exist_ok=True)
+
+            image_list = sorted(glob(os.path.join(data_folder, '*_IMG*')))
+            label_list = sorted(glob(os.path.join(data_folder, '*_LBL*'))) 
+
+            txt_filename = os.path.join(output_path, stage +'_list.txt')
+            write2txt(txt_filename, image_list, label_list)
+    else:
+        # cross_validation
+        data_folder = 'location of raw data'
+        output_path = 'location of prepared data'
         os.makedirs(output_path, exist_ok=True)
-        
+
+        _NUM_FOLDS_ = 4 
         image_list = sorted(glob(os.path.join(data_folder, '*_IMG*')))
-        label_list = sorted(glob(os.path.join(data_folder, '*_LBL*'))) 
+        label_list = sorted(glob(os.path.join(data_folder, '*_LBL*')))
 
-        txt_filename = os.path.join(output_path, stage +'_list.txt')
-        write2txt(txt_filename, image_list, label_list)
+        kf = KFold(n_splits=_NUM_FOLDS_, random_state=42, shuffle=True) 
+        for fold in range(_NUM_FOLDS_):
+            for i, (train_idx, test_idx) in enumerate(kf.split(range(len(image_list)))):
+                if i == fold:
+                    train_index = train_idx
+                    test_index = test_idx
+                    break
+            train_image_list = list(itemgetter(*train_index)(image_list)) 
+            test_image_list = list(itemgetter(*test_index)(image_list)) 
+            train_mask_list = list(itemgetter(*train_index)(label_list)) 
+            test_mask_list = list(itemgetter(*test_index)(label_list)) 
 
-    # cross_validation
-    # data_folder = '/media/Ningning/PJ_Pelvis/image_pair_crop'
-    # output_path = '/media/Ningning/PJ_Pelvis/main/data/hiatus'
-    # os.makedirs(output_path, exist_ok=True)
-    
-    # _NUM_FOLDS_ = 4 
-    # image_list = sorted(glob(os.path.join(data_folder, '*_IMG*')))
-    # label_list = sorted(glob(os.path.join(data_folder, '*_LBL*')))
-
-    # kf = KFold(n_splits=_NUM_FOLDS_, random_state=42, shuffle=True) 
-    # for fold in range(_NUM_FOLDS_):
-    #     for i, (train_idx, test_idx) in enumerate(kf.split(range(len(image_list)))):
-    #         if i == fold:
-    #             train_index = train_idx
-    #             test_index = test_idx
-    #             break
-    #     train_image_list = list(itemgetter(*train_index)(image_list)) 
-    #     test_image_list = list(itemgetter(*test_index)(image_list)) 
-    #     train_mask_list = list(itemgetter(*train_index)(label_list)) 
-    #     test_mask_list = list(itemgetter(*test_index)(label_list)) 
-
-    #     train_filename = os.path.join(output_path, 'f'+str(fold)+'_train_list.txt')
-    #     test_file_name = os.path.join(output_path, 'f'+str(fold)+'_test_list.txt')
-    #     write2txt(train_filename, train_image_list, train_mask_list)
-    #     write2txt(test_file_name, test_image_list, test_mask_list)
+            train_filename = os.path.join(output_path, 'f'+str(fold)+'_train_list.txt')
+            test_file_name = os.path.join(output_path, 'f'+str(fold)+'_test_list.txt')
+            write2txt(train_filename, train_image_list, train_mask_list)
+            write2txt(test_file_name, test_image_list, test_mask_list)
         
 
 
